f0883139f9179809121835eb18a955331118387a
Ion Bernic <bernic.ion@gmail.com>
2022-03-18 15:11:06 +0200

    0: Implementation of "Adapter(Structural)" design pattern.

---
+++ src/com/bernic/patterns/structural/adapter/site/RunAdapterExample.java
@@ -0,0 +1,28 @@
+package com.bernic.patterns.structural.adapter.site;
+
+import com.bernic.patterns.structural.adapter.payd.PayD;
+import com.bernic.patterns.structural.adapter.xpay.Xpay;
+
+public class RunAdapterExample {
+
+    public static void main(String[] args) {
+        Xpay xpay = new XpayImpl();
+        xpay.setCreditCardNo("4789565874102365");
+        xpay.setCustomerName("Max Warner");
+        xpay.setCardExpMonth("09");
+        xpay.setCardExpYear("25");
+        xpay.setCardCVVNo((short)235);
+        xpay.setAmount(2565.23);
+
+        PayD payD = new XpayToPayDAdapter(xpay);
+        testPayD(payD);
+    }
+
+    private static void testPayD(PayD payD){
+        System.out.println(payD.getCardOwnerName());
+        System.out.println(payD.getCustCardNo());
+        System.out.println(payD.getCardExpMonthDate());
+        System.out.println(payD.getCVVNo());
+        System.out.println(payD.getTotalAmount());
+    }
+}
